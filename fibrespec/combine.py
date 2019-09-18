24c133d30da26d40159f5210cbc8a081bf4595d2
Anthony Horton <anthony.horton@aao.gov.au>
2019-09-19 03:54:46 +1000

    New spectral extraction function, removed redundant make_flat

--- fibrespec/combine.py
+++ fibrespec/combine.py
@@ -207,38 +207,6 @@ def process_fits(fitspath, *,
     return master
 
 
-def make_flat(directory,
-              master_darks,
-              window,
-              max_value=30000,
-              min_value=10000,
-              low_thresh=3,
-              high_thresh=3):
-    exposure_times = list(master_darks.keys())
-    ifc = ImageFileCollection(directory, keywords=['obstype', 'totalexp'])
-    images = []
-    for ccddata in ifc.ccds(obstype='SFLAT', ccd_kwargs={'unit': 'adu'}):
-        if ccddata.header['totalexp'] in exposure_times:
-            ccddata = trim_image(ccddata[window[1]:window[3]+1, window[0]:window[2]+1])
-            nnpf = np.percentile(ccddata.data, 99.5)
-            if nnpf < max_value and nnpf > min_value:
-                ccddata = subtract_dark(ccddata,
-                                        master_darks[ccddata.header['totalexp']],
-                                        exposure_time='totalexp',
-                                        exposure_unit=u.second)
-                ccddata = ccddata.divide(np.ma.average(ccddata.data))
-                images.append(ccddata)
-    s = "Found {} files with exposures in {} & 99.5 percentiles in {}-{}.".format(len(images),
-                                                                                  exposure_times,
-                                                                                  min_value,
-                                                                                  max_value)
-    print(s)
-    combiner = Combiner(images)
-    combiner.sigma_clipping(low_thresh=low_thresh, high_thresh=high_thresh, func=np.ma.average)
-    master_flat = combiner.average_combine()
-    return master_flat
-
-
 def mask_bad_pixels(image_data, pixel_mask):
     try:
         bad_pixels = np.loadtxt(pixel_mask, dtype=np.int)
--- fibrespec/extract.py
+++ fibrespec/extract.py
@@ -3,6 +3,56 @@ from warnings import warn
 import numpy as np
 
 
+def extract_spectra(image_data, *, traces, width, axis, reverse_spectra=False):
+    """Extract fibre spectra from image data.
+
+    Parameters
+    ----------
+    image_data: ccdproc.CCDData
+        Image containing the fibre spectra.
+    traces: astropy.modeling.Model
+        Model set that when evaluated at a given pixel coordinate along the spectral axis will
+        return the pixel coordinates of the fibre spectra centres in the other axis, such as
+        that returned by fibrespec.trace.trace_fibres().
+    width: int
+        Width, in pixels, of the spectral extraction regions.
+    axis: int
+        Axis of the image which corresponds to the spectral direction.
+
+    Returns
+    -------
+    spectra: np.ndarray
+        Spectra.
+
+    """
+    spectral_length = image_data.data.shape[axis]
+    n_spectra = len(traces)
+
+    xs = np.arange(0, spectral_length)
+
+    ys = traces(np.broadcast_to(xs, (n_spectra, spectral_length)))
+    ys = np.expand_dims(ys, axis=2)
+    ys = ys + np.arange(-(width - 1)/2, (width + 1)/2).reshape((1, 1, width))
+    ys = np.around(ys, decimals=0).astype(np.int)
+
+    xs = xs.reshape((1, spectral_length, 1))
+    xs = np.broadcast_to(xs, (n_spectra, spectral_length, width)).astype(np.int)
+
+    if axis == 0:
+        pixel_values = image_data.data[(xs.ravel(), ys.ravel())]
+    elif axis == 1:
+        pixel_values = image_data.data[(ys.ravel(), xs.ravel())]
+    else:
+        raise ValueError("axis must be 0 or 1, got {}.".format(axis))
+
+    pixel_values = pixel_values.reshape((n_spectra, spectral_length, width))
+    spectra = pixel_values.sum(axis=2)
+    if reverse_spectra:
+        spectra = np.flip(spectra, axis=1)
+
+    return spectra
+
+
 def initialise_tramlines(tram_coef_file, window, width, background_width=None):
     """Used tramline fit from tram_coef to produce a list of tuples of arrays, each tuple contains
     the y and x coordinates of the pixels to include in the extraction for a given fibre. When
@@ -13,7 +63,9 @@ def initialise_tramlines(tram_coef_file, window, width, background_width=None):
         background_width (int): total width, in pixels, of the spectral extraction regions including
             the instrument background subtraction regions.
 
-    Returns:
+    Returns: []
+
+    for (a, b, c) in t
         tramlines (list of tuples of np.array): pixels coordinates for each fibre
         tramlines_bg (list of tuples of np.array): pixel coordinates for each fibre, including
             background regions.
